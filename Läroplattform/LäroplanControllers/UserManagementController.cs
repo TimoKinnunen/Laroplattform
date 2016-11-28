fb165ad994bc20fe98d2a9ef28ac6cea6e715fee
TimoKinnunen <timo.kinnunen@bredband.net>
2016-11-28 14:07:22 +0100

    UserManagement

---
+++ Läroplattform/LäroplanControllers/UserManagementController.cs
@@ -0,0 +1,45 @@
+﻿using Läroplattform.Helpers;
+using Läroplattform.LäroplanViewModels;
+using Läroplattform.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Läroplattform.LäroplanControllers
+{
+    public class UserManagementController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public ActionResult Index()
+        {
+            ViewBag.CurrentUserMessage = string.Empty;
+            if (User.IsInRole("lärare"))
+            {
+                var currentApplicationUser = HelpUser.GetCurrentApplicationUser(User);
+
+                var firstName = currentApplicationUser.FirstName;
+                var lastName = currentApplicationUser.LastName;
+                var email = currentApplicationUser.Email;
+
+                TeacherSchema teacherSchema = new TeacherSchema
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Email = email
+                };
+
+                ViewBag.CurrentUserMessage = teacherSchema.FullName + " " + email;
+
+                var allRoles = db.Roles.ToList();
+                var allUsers = db.Users.ToList();
+
+                return View();
+            }
+            else
+            {
+                ViewBag.CurrentUserMessage = "Teacher has not logged in yet!";
+                return View();
+            }
+        }
+    }
+}
