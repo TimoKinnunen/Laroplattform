26a468305b8ad2f58000d8d97eff8f3883ddca47
timok <timok@TIMOSURFACEPRO4>
2016-11-30 01:05:44 +0100

    Test igen

--- Läroplattform/Migrations/Configuration.cs
+++ Läroplattform/Migrations/Configuration.cs
@@ -6,6 +6,7 @@ namespace Läroplattform.Migrations
     using Microsoft.AspNet.Identity.EntityFramework;
     using Models;
     using System;
+    using System.Data.Entity;
     using System.Data.Entity.Migrations;
 
     internal sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
@@ -17,86 +18,87 @@ namespace Läroplattform.Migrations
 
         protected override void Seed(ApplicationDbContext context)
         {
-            var roleStore = new RoleStore<IdentityRole>(context);
-            var roleManager = new RoleManager<IdentityRole>(roleStore);
 
-            ApplicationUser applicationUser;
-            IdentityResult identityResult;
+            //var roleStore = new RoleStore<IdentityRole>(new ApplicationDbContext());
+            //var roleManager = new RoleManager<IdentityRole>(roleStore);
 
-            if (!roleManager.RoleExists("Lärare"))
-            {
-                identityResult = roleManager.Create(new IdentityRole(("Lärare")));
-                if (!identityResult.Succeeded)
-                {
-                    throw new Exception("Error creating role 'Lärare' in seed.");
-                }
-            }
+            //ApplicationUser applicationUser;
+            //IdentityResult identityResult;
 
-            if (!roleManager.RoleExists("Elev"))
-            {
-                identityResult = roleManager.Create(new IdentityRole(("Elev")));
-                if (!identityResult.Succeeded)
-                {
-                    throw new Exception("Error creating role 'Elev' in seed.");
-                }
-            }
+            //if (!roleManager.RoleExists("Lärare"))
+            //{
+            //    identityResult = roleManager.Create(new IdentityRole(("Lärare")));
+            //    if (!identityResult.Succeeded)
+            //    {
+            //        throw new Exception("Error creating role 'Lärare' in seed.");
+            //    }
+            //}
 
-            var userStore = new UserStore<ApplicationUser>(context);
-            var userManager = new UserManager<ApplicationUser>(userStore);
+            //if (!roleManager.RoleExists("Elev"))
+            //{
+            //    identityResult = roleManager.Create(new IdentityRole(("Elev")));
+            //    if (!identityResult.Succeeded)
+            //    {
+            //        throw new Exception("Error creating role 'Elev' in seed.");
+            //    }
+            //}
 
-            applicationUser = userManager.FindByEmail("stina.larsson@lexicon.se");
-            if (applicationUser == null)
-            {
-                ApplicationUser newApplicationUser =
-                    new ApplicationUser
-                    {
-                        FirstName = "Stina",
-                        LastName = "Larsson",
-                        UserName = "stina.larsson@lexicon.se",
-                        Email = "stina.larsson@lexicon.se",
-                        TimeOfRegistration = DateTime.Now
-                    };
-                identityResult = userManager.Create(newApplicationUser, "Lexicon01!");
-                if (identityResult.Succeeded)
-                {
-                    identityResult = userManager.AddToRole(newApplicationUser.Id, "Lärare");
-                    if (!identityResult.Succeeded)
-                    {
-                        throw new Exception("Error adding user 'stina.larsson@lexicon.se' to role 'Lärare' in seed.");
-                    }
-                }
-                else
-                {
-                    throw new Exception("Error creating user 'stina.larsson@lexicon.se' in seed.");
-                }
-            }
+            //var userStore = new UserStore<ApplicationUser>(new ApplicationDbContext());
+            //var userManager = new UserManager<ApplicationUser>(userStore);
 
-            applicationUser = userManager.FindByEmail("goran.persson@lexicon.se");
-            if (applicationUser == null)
-            {
-                ApplicationUser newApplicationUser =
-                    new ApplicationUser
-                    {
-                        FirstName = "Göran",
-                        LastName = "Persson",
-                        UserName = "goran.persson@lexicon.se",
-                        Email = "goran.persson@lexicon.se",
-                        TimeOfRegistration = DateTime.Now
-                    };
-                identityResult = userManager.Create(newApplicationUser, "Lexicon01!");
-                if (identityResult.Succeeded)
-                {
-                    identityResult = userManager.AddToRole(newApplicationUser.Id, "Elev");
-                    if (!identityResult.Succeeded)
-                    {
-                        throw new Exception("Error adding user 'goran.persson@lexicon.se' to role 'Elev' in seed.");
-                    }
-                }
-                else
-                {
-                    throw new Exception("Error creating user 'goran.persson@lexicon.se' in seed.");
-                }
-            }
+            //applicationUser = userManager.FindByEmail("stina.larsson@lexicon.se");
+            //if (applicationUser == null)
+            //{
+            //    ApplicationUser newApplicationUser =
+            //        new ApplicationUser
+            //        {
+            //            FirstName = "Stina",
+            //            LastName = "Larsson",
+            //            UserName = "stina.larsson@lexicon.se",
+            //            Email = "stina.larsson@lexicon.se",
+            //            TimeOfRegistration = DateTime.Now
+            //        };
+            //    identityResult = userManager.Create(newApplicationUser, "Lexicon01!");
+            //    if (identityResult.Succeeded)
+            //    {
+            //        identityResult = userManager.AddToRole(newApplicationUser.Id, "Lärare");
+            //        if (!identityResult.Succeeded)
+            //        {
+            //            throw new Exception("Error adding user 'stina.larsson@lexicon.se' to role 'Lärare' in seed.");
+            //        }
+            //    }
+            //    else
+            //    {
+            //        throw new Exception("Error creating user 'stina.larsson@lexicon.se' in seed.");
+            //    }
+            //}
+
+            //applicationUser = userManager.FindByEmail("goran.persson@lexicon.se");
+            //if (applicationUser == null)
+            //{
+            //    ApplicationUser newApplicationUser =
+            //        new ApplicationUser
+            //        {
+            //            FirstName = "Göran",
+            //            LastName = "Persson",
+            //            UserName = "goran.persson@lexicon.se",
+            //            Email = "goran.persson@lexicon.se",
+            //            TimeOfRegistration = DateTime.Now
+            //        };
+            //    identityResult = userManager.Create(newApplicationUser, "Lexicon01!");
+            //    if (identityResult.Succeeded)
+            //    {
+            //        identityResult = userManager.AddToRole(newApplicationUser.Id, "Elev");
+            //        if (!identityResult.Succeeded)
+            //        {
+            //            throw new Exception("Error adding user 'goran.persson@lexicon.se' to role 'Elev' in seed.");
+            //        }
+            //    }
+            //    else
+            //    {
+            //        throw new Exception("Error creating user 'goran.persson@lexicon.se' in seed.");
+            //    }
+            //}
 
             context.ActivityTypes.AddOrUpdate(
               p => p.Name,
